c7f3fae34a36f6eabdb6c97c9ed68c1ed716eaeb
M.Mohtashim <mmohtashim59@gmail.com>
2020-12-03 05:57:27 -0800

    import file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Bulb from './Bulb';
+import './App.css';
 
 function App() {
   return (
     <div>
-      <h1> Hello World </h1>
+      <Bulb />
     </div>
   )
 }
