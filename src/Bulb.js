a2ade860be97c06f6e1bcee639ffd8f1d1fd0bc8
M.Mohtashim <mmohtashim59@gmail.com>
2020-12-03 06:31:34 -0800

    useState Hook

--- src/Bulb.js
+++ src/Bulb.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import img from "./img/bulb.jpg"
 
 function Bulb() {
+
+    const [turnOn, setTurnOn] = useState(false)
+    const [turnOff, setTurnOff] = useState(false)
+    const [flicker, setFlicker] = useState(false)
 
 
     return (
